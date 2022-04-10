60b8685afd61fae820ab1196c0997c76e7fef8cc
Bibest <Bibhestee@gmail.com>
2022-04-10 05:34:12 +0100

    [All updated]

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - entry point where the program start
  *
  * Return: Always (Success)
  */
-
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main
+  * main - entry point where the program start
   *
   * Return: Always (Success)
   */
