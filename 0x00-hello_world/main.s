a31c73599060d5fb1e3acb94cd01b0dfe9150c4c
Bibest <Bibhestee@gmail.com>
2022-04-10 15:00:40 +0100

    Add: 100-intel added.

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,38 +1,20 @@
-	.file	"main.c"
 	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
+	.file	"main.c"
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
+// %bb.0:
+	sub	sp, sp, #16                     // =16
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	mov	w0, wzr
+	str	wzr, [sp, #12]
+	add	sp, sp, #16                     // =16
 	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
 	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+                                        // -- End function
+	.ident	"clang version 13.0.1"
+	.section	".note.GNU-stack","",@progbits
