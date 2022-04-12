ecc6e4fc229a55e317a2c26dfa0b422196c0edcb
Bibest <Bibhestee@gmail.com>
2022-04-12 22:06:05 +0100

    Add: _putchar file added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - entry point: Its the start point of the program.
+* return - return null
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programming
+Starting a low level language as a software engineering student (C programming).
