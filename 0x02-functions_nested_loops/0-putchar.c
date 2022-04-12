480974f45046e2dbcbe59634778eff0195c2485c
Bibest <Bibhestee@gmail.com>
2022-04-12 22:39:46 +0100

    [Edited]: 0-putchar edited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,7 @@
 * main - entry point: Its the start point of the program.
 * return - return null
 * main(void) - return the main function void
+* return(0) - The program return 0(zero)
 */
 int main(void)
 {
