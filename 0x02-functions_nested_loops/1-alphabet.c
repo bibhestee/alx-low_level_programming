ea39488719407d191fb18ab9e2d9864e181aa81d
Bibest <Bibhestee@gmail.com>
2022-04-13 18:18:24 +0100

    [Updated]- 1-alphabet .c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
 #include "main.h"
-/*
-* main() - entry point: Its the starting point of the program
-* main.h - include the created library
-* return - Always 0
-*/
-int main(void)
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
 {
-	char alphabet = 'a';
-	print_alphabet(alphabet);
-	return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
