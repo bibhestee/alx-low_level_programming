528bc101ef84bae2330cfe11820e231988bd8a85
Bibest <Bibhestee@gmail.com>
2022-04-13 18:06:59 +0100

    Unknown

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point where the program start to execute.
+*
+* return - return the function and stop the program */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(0);
+}
