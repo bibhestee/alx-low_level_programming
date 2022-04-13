a668f14cdbec8cd78f93314b90a68f86065ad8d6
Bibest <Bibhestee@gmail.com>
2022-04-13 18:50:07 +0100

    [Added] 2-print_alphabet_x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+* main.h - Includes the functions and prototypes
+* return 0
+*/
+int main(void)
+{
+// Declares variable i for repeating the task x10
+	int i;
+/
+	for(i = 1;i <= 10; i++)
+{
+// For each iteration- print all alphabets followed by newline
+	char c = 'a';
+	while( c <= 'z' )
+	{
+		_putchar(c);
+		c++;
+// Return value of variable c and repeat the process till c = z
+		return c;
+	}
+	_putchar("\n");
+// Return value of variable i and repeat the process till i = 10
+	return i;
+}
+	return (0);
+}
+
