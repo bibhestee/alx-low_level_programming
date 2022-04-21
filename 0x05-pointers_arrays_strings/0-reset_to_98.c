b515c5e49f8590e78f8d04811b9aa10556e49fdc
bibest <bibhestee@ggmail.com>
2022-04-20 22:58:21 -0700

    Tasks updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
- ​#​include​ ​"​main.h​"
- ​/*​*
- ​ * reset_to_98 - reset argument to 98
- ​ * @n: pointer
- ​ * Return: void
- ​*/
+#include "main.h"
 
- ​void​ ​reset_to_98​(​int​ *n)
- ​{
+/*
+* reset_to_98 - reset argument to 98
+* @n - variable that holds the argument(pointer)
+* return - Returns 0
+*/
+
+void reset_to_98(int *n)
+{
 	*n = 98;
- ​}
+}
