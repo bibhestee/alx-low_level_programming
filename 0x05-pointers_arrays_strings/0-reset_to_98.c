e62f30ce2048f11d4fedc432474ceff30f192b99
bibest <bibhestee@ggmail.com>
2022-04-20 23:36:48 -0700

    0-reset_to_98 and 1-swap file modified

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* reset_to_98 - reset argument to 98
-* @n - pointer
-* return - Returns 0
-*/
+ * reset_to_98 - reset the pointer value to 98
+ * @n: pointer 1
+ */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-* swap_int - swap int
-* @a - pointer 1
-* @b - pointer 2​
-* Return - void
-*/
+ * swap_int - swap integers
+ * @a: pointer 1
+ * @b: pointer 2​
+ */
 
 void swap_int(int *a, int *b)
 {
