408d9b5c38bbf327d176f451a27260a609a85451
bibest <bibhestee@ggmail.com>
2022-04-23 13:19:05 -0700

    0-strcat file added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
 /**
- * 
+ * _strcat - get two strings
+ * @dest: pointer 1
+ * @src: pointer 2
+ * Return: return concatination of the two pointers
  */
 
+char _strcat(char *dest, char *src)
+{
+	return (*dest + *src);
+}
