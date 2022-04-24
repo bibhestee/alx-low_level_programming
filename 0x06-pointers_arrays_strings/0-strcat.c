752d67bc5b476d255ac96264ff27af1ff02d89ee
Bibest <Bibhestee@gmail.com>
2022-04-24 11:01:34 +0100

    File 1 and 2 added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,24 @@
 
 char _strcat(char *dest, char *src)
 {
-	return (*dest + *src);
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++:
+	}
+
+	b = 0:
+
+	while (src[b] != 0)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
 }
+
