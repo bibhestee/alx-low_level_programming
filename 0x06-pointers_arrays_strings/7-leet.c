1a2c98e1b1209cd3fd76c4f2d7ef42043de1da75
Bibest <Bibhestee@gmail.com>
2022-04-25 22:06:44 +0100

    7-leet file added.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+char *leet(char *)
+{
+	int x = 0;
+
+	while ( s[x] != 0)
+	{
+		if (s[x] = 'a' || s[x] = 'A')
+		{
+			s[x] = 4;
+		}
+		else if(s[x] = 'e' || s[x] = 'E')
+		{
+			s[x] = 3:
+		}
+		else if(s[x] = 'o' || s[x] = 'O')
+		{
+			s[x] = 0;
+		}
+		else if(s[x] = 't'  || s[x] = 'T')
+
+		{
+			s[x] = 7;
+		}
+		else if (s[x] = 'l' || s[x] = 'L')
+		{
+			s[x] = 1;
+		}
+		else
+		{
+			continue;
+		}
+			x++;
+	}
+	return s[];
+}
+
