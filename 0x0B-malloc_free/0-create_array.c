a227916e0670dc3e2aa763229bb946af7da61906
Bibest <Bibhestee@gmail.com>
2022-05-05 10:29:41 +0100

    README Added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *create_array - A function which creates an array with malloc
+ *@size: memory size
+ *@c: character variable
+ *Return: return a pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *create;
+	if(size = 0)
+	{
+		return (null);
+	}
+	else{
+	create[] = malloc(size);
+	}
+	**create = c;
+	return (create);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+C - Automatic or Dynamic Allocation
