9b0ef261933a7c46f32e1cf5dda17c0000ba86e8
Bibest <Bibhestee@gmail.com>
2022-05-05 22:34:24 +0000

    2-str_concat.c Added

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,11 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int i = 0;
 	unsigned int j = 0;
-	char *c0 = malloc(sizeof(char));
+	char *c0 = malloc(10 * sizeof(char));
 
 	if (c0 == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	while (*(s1 + i) != '\0')
 		i++;
