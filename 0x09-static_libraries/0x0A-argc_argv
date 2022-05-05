416a39a51717e85e6f49ad8e35750b3f34d6420d
Bibest <Bibhestee@gmail.com>
2022-05-05 08:24:07 +0000

    Good to go

---
+++ 0x09-static_libraries/0x0A-argc_argv
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - entry point: Its the start point of the program.
+* return - return null
+* main(void) - return the main function void
+* return(0) - The program return 0(zero)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
-char *_strncpy(char *dest, char *src, int n){
-	return dest;
+/**
+ * _strncpy - copy a string from one variable to another
+ *@dest: variable 1
+ *@src: variable 2
+ *@n: gives the count of value src and dest arrays
+ *Return: return the value of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
 }
