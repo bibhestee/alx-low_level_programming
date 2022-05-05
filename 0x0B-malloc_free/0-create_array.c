c04a7d8d96e1d9bb31a5d9329607072ee7c20326
Bibest <Bibhestee@gmail.com>
2022-05-05 18:44:20 +0100

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char (*c0) = malloc(size);
 
-	if (c0 == NULL)
+	if (c0 == NULL || c0 == 0)
 	{
 		return (NULL);
 	}
 	while (i <= size)
 	{
-		*c0 = c;
+		*c0 = &c;
 		i++;
 	}
 	return (c0);
