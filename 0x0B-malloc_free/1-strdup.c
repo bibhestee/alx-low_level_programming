2fcc707b04780f20b71f42702768d24fdd078544
Bibest <Bibhestee@gmail.com>
2022-05-05 20:29:06 +0100

    1-strdup Edited

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,23 +4,18 @@
 /**
  *_strdup - duplicate string to a pointer
  *@str: pointer 1
- *@c0: pointer with allocated memory and string duplicate
  *Return: return null or character
  */
 
 char *_strdup(char *str)
 {
 	char *c0 = malloc(sizeof(char));
+
 	if (c0 == NULL || str == 0)
 	{
 		return (NULL);
 	}
-	while (str != NULL)
-	{
-		int i = 0;
-		*(c0 + i) = *str;
-		i++;
-	}
+	c0 = str;
 	return (c0);
 }
 
