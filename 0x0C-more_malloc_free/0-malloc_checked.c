5cbaa6793217e645a5e8e3019940cfb717527adf
Bibest <Bibhestee@gmail.com>
2022-05-09 16:17:36 +0000

    Project started...

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocate a memory space and returns to a pointer
+ *exit: exit to 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+C - More Malloc Free
+
+0-malloc checked
+1-string nconcat
+2-calloc
+3-array range
