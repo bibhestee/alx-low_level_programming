040e950d7110752a6bff23f8e40a8dceac649f6e
Bibest <Bibhestee@gmail.com>
2022-05-10 08:56:58 +0000

    Task 2 Completed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 
 /**
- *ptr_m - allocates a memory to the arguments passed
- *Return: Return NULL on failure
+ *_calloc - allocates a memory using malloc
+ *and number of bytes passed as argument
+ *@nmemb: number of elements in the array
+ *@size: size in bytes of the elements
+ *
+ *Return: Return NULL on failure or pointer ptr_m
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr_m;
+	char *ptr_m;
+	unsigned int i;
 
 	ptr_m = malloc(size * nmemb);
 	if (ptr_m == NULL || nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	ptr_m = 0;
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr_m[i] = 0;
+	}
 	return (ptr_m);
 }
