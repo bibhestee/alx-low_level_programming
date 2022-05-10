1fdfb3dbb79eaff546b926966453ab6c0c1d2b59
Bibest <Bibhestee@gmail.com>
2022-05-10 08:47:07 +0000

    Task 1 Completed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-0x0C-more_malloc_free#include "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -15,28 +15,40 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL)
+	{
 		i = 0;
+	}
 	else
 	{
 		for (i = 0; s1[i]; i++)
-			;
+		;
 	}
 	if (s2 == NULL)
+	{
 		j = 0;
+	}
 	else
 	{
 		for (j = 0; s2[j]; j++)
-			;
+		;
 	}
 	if (j > n)
+	{
 		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
+		s = malloc(sizeof(char) * (i + j + 1));
+	}
 	if (s == NULL)
+	{
 		return (NULL);
+	}
 	for (k = 0; k < i; k++)
+	{
 		s[k] = s1[k];
+	}
 	for (k = 0; k < j; k++)
+	{
 		s[k + i] = s2[k];
+	}
 	s[i + j] = '\0';
 	return (s);
 }
