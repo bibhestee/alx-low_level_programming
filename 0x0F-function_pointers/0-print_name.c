7d209d01771f0110064a7a61cda5d234f7344fbb
Bibest <Bibhestee@gmail.com>
2022-05-13 11:03:19 +0000

    Initial files added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ *
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*f)(char *) = &_putchar;
+	
+}
+
+/**
+Task - Create a function that prints a name
+name is of data type pointer to a character i.e its a string
+
+
+*/
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+C - 0x0F. Function pointers
+
+This folder contains the task on function pointers
