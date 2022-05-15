edd7749fd46db613951a1fc2995d0ce89d1a01ce
bibest <bibhestee@ggmail.com>
2022-05-15 11:06:31 -0700

    Task 2 Completed!

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,21 +13,21 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	for (i = 0;i < size; i++)
+	if (size <= 0)
 	{
-		if (size <= 0)
-		{
-			exit (-1);
-		}
-		if ((*cmp)(array[i]) != 0)
+		return (-1);
+	}
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
 		{
 			return (i);
 		}
-		else if ((*cmp)(array[i]) == 0)
-		{
-			exit (-1);
-		}
 	}
-	return (i);
+	return (-1);
 }
