2291347951677c871719313158daf185494d15c7
Bibest <Bibhestee@gmail.com>
2022-05-16 15:50:42 +0000

    Task 0 - Completed!

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,7 +11,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int i;
-	int sum = 0;
+	int add = 0;
 
 	va_list ap;
 
@@ -23,8 +23,8 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(ap, int);
+		add = add + va_arg(ap, int);
 	}
 	va_end(ap);
-	return (0);
+	return (add);
 }
