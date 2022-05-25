e49a9b3d132c62fa069000c946f88f2c8c3c752b
Bibest <Bibhestee@gmail.com>
2022-05-25 11:40:00 +0000

    Task 1 - Checking...

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - a function that returns the number of elements
+ * in a linked list_t list.
+ * @h: a pointer to a constant list_t list.
+ * Description: a pointer to a constant can't change the value
+ * of the variable it is pointing to.
+ *
+ * Return: the total number of elements in a linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	list_t *ptr = NULL;
+
+	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		exit(0);
+	if (h == NULL)
+		return (0);
+
+	ptr->next = h->next;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
