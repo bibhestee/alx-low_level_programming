b355fb56b4d74414cb5bcb738f9b45f09ee95e34
Bibest <Bibhestee@gmail.com>
2022-07-07 14:57:15 +0000

    Modify 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
 	/* Create new node */
 	node->n = n;
 	node->prev = NULL;
