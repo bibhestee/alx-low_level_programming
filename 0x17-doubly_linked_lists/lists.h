37bd75b21cc8433751dadefaeafc01c83fb81d75
Bibest <Bibhestee@gmail.com>
2022-07-10 16:58:55 +0000

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -16,6 +16,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
 #include <stddef.h>
 
 size_t print_dlistint(const dlistint_t *h);
@@ -23,5 +24,6 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif
