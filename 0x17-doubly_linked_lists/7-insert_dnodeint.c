94919951a13a4fb62c5a4f72fd12e4c66f201d88
Bibest <Bibhestee@gmail.com>
2022-07-10 18:27:01 +0000

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (current != NULL)
 	{
 		ptr = ptr->next;
-		if (count == idx)
+		if (count == idx - 1)
 		{
 			current->next = node;
 			node->prev = current;
