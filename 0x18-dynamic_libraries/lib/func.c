24cd636d6a7027ac30df3fbb26d2d5e23794eff0
Bibest <Bibhestee@gmail.com>
2022-07-13 10:08:05 +0000

    Lib dir for task 2

---
+++ 0x18-dynamic_libraries/lib/func.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+/**
+ * add - performs addition operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of operation
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
+/**
+ * sub - performs substraction operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of operation
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - performs multiplication operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * div - performs division operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of operation
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - performs modulus operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of operation
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
