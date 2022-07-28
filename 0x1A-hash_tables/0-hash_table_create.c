35b069bafe15d41627bae777a6a0917a331eedeb
Bibest <Bibhestee@gmail.com>
2022-07-28 14:37:02 +0000

    Create and Update 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table of the size specified
+ * @size: size of the hash table creatd
+ * Description: Hash table of size specified is created
+ *                and each has two members.
+ * The first member is the size i.e to represent the index of the key.
+ * The second member is the pointer to the first element on that index.
+ *
+ * Return: Returns a pointer to the hash_table
+ *          or NULL if memory is not allocated.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t) * size);
+
+	/* Check if hash_table is successfully created */
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
+
+	return (hash_table);
+}
