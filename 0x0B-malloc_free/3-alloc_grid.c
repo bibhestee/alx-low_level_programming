90bca783375df5efafc70f1a687b9ba11990741e
Bibest <Bibhestee@gmail.com>
2022-09-26 19:40:24 +0000

    Create a 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,13 +16,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int **)malloc(sizeof(int *) * width);
+	ptr = (int **)malloc(sizeof(int *) * height);
 
-	for (i = 0; i < width; i++)
-		ptr[i] = (int *)malloc(sizeof(int) * height);
+	for (i = 0; i < height; i++)
+		ptr[i] = (int *)malloc(sizeof(int) * width);
 
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 
 	return (ptr);
