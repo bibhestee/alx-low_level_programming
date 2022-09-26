e42f22756f65f89fb4b1eea9ce7bfdfc51581f7e
Bibest <Bibhestee@gmail.com>
2022-09-26 12:48:10 +0000

    Function that concat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,36 +13,61 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, len_1, len_2, total;
 	char *s;
 
-	len_1 = strlen(s1);
-	len_2 = strlen(s2);
-	total = len_1 + len_2 + 1;
-
-	s = (char *)malloc(sizeof(char) * (total));
-
-	if (!s)
-		return (NULL);
-	/* Add the first str */
-	if (s1)
+	if (!s1 && !s2)
 	{
+		return (NULL);
+	}
+	else if (!s1 && s2)
+	{
+		return (dup(s2));
+	}
+	else if (s1 && !s2)
+	{
+		return (dup(s1));
+	}
+	else
+	{
+		len_1 = strlen(s1);
+		len_2 = strlen(s2);
+		total = len_1 + len_2 + 1;
+
+		s = (char *)malloc(sizeof(char) * (total));
+
+		if (!s)
+			return (NULL);
+
+		/* Add the first str */
 		for (i = 0; i <= len_1; i++)
 		{
 			s[i] = s1[i];
 		}
-	}
-	else if (!s1 && s2)
-	{
-		for (i = 0; i <= len_2; i++)
+
+		/* Concat the second str */
+		for (i = len_1, j = 0; i <= total; i++, j++)
 		{
-			s[i] = s2[i];
+			s[i] = s2[j];
 		}
 		return (s);
 	}
-	if (!s2)
-		return (s);
-	/* Concat the second str */
-	for (i = len_1, j = 0; i <= total; i++, j++)
+}
+
+/**
+ * dup - make a duplicate of the input string
+ * @a: input string
+ *
+ * Return: pointer to the duplicate
+ */
+
+char *dup(char *a)
+{
+	unsigned int i, len;
+	char *s;
+
+	len = strlen(a);
+	s = (char *)malloc(sizeof(char) * len);
+	for (i = 0; i <= len; i++)
 	{
-		s[i] = s2[j];
+		s[i] = a[i];
 	}
 	return (s);
 }
