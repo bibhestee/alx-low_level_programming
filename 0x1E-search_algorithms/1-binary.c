3ad608c714b306fec0957e0728eb0bf9fc380faa
Bibest <bibhestee@gmail.com>
2022-10-31 21:58:08 +0100

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,79 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value
+ *	in a sorted array of integers
+ *	using the Binary search algorithm.
+ * @array: a pointer to the first element
+ *	 of the array to search in.
+ * @size: the number of elements in array
+ * @value: the value to search for.
+ *
+ * Return: return the first index where
+ * 	value is located.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (!array)
+		return (-1);
+
+	return (binary(array, 0, size - 1, value));
+}
+
+/**
+ * binary - searches for a value
+ *      in a sorted array of integers
+ *      using the Binary search algorithm.
+ * @array: a pointer to the first element
+ *       of the array to search in.
+ * @lo: starting point of the array to be searched
+ * @hi: ending point of the array to be searched
+ * @value: the value to search for.
+ *
+ * Return: return the first index where
+ *      value is located.
+ */
+
+int binary(int *array, size_t lo, size_t hi, int value)
+{
+	size_t mid = (hi + lo)/ 2;
+
+	if (!array)
+		return (-1);
+
+	print_array(array, lo, hi);
+
+	if (lo == hi)
+		return (-1);
+	if (array[mid] == value)
+	{
+		return (value);
+	}
+	else if (array[mid] > value)
+	{
+		return (binary(array, lo, mid - 1, value));
+	}
+	else
+	{
+		return (binary(array, mid + 1, hi, value));
+	}
+}
+
+/**
+ * print_array - print the array
+ * @array: A pointer to the array
+ * @lo: the starting point of the array
+ * @hi: the ending point of the array
+ *
+ */
+
+void print_array(int *array, size_t lo, size_t hi)
+{
+	printf("Searching in array: ");
+	for (; lo < hi; lo++)
+	{
+		printf("%d, ", array[lo]);
+	}
+	printf("%d\n", array[hi]);
+}
