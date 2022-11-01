73cd76b1077c56bece18c5be9f86c8c121cf4a0d
Bibest <bibhestee@gmail.com>
2022-11-01 16:50:35 +0000

    Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -8,7 +8,7 @@
  * @size: the number of elements in array.
  * @value: the value to search for.
  *
- * Return: the first index where value is located
+ * Return: the first index where value is located.
  */
 
 int jump_search(int *array, size_t size, int value)
