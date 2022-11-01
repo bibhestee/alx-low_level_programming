bc7824d8bb7b2aa2b49b9686ff033dadb8f34f4e
Bibest <bibhestee@gmail.com>
2022-11-01 13:32:53 +0100

    add new prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary(int *array, size_t lo, size_t hi,  int value);
 void print_array(int *array, size_t lo, size_t hi);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*----SEARCH ALGOS-----*/
